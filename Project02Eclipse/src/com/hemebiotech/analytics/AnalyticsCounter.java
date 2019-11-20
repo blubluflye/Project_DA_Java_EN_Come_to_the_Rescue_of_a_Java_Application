6630f2b35d1c75db4c5972350d7d2bb404731611
Nicopatsch <nicolas.patschkowski@telecom-sudparis.eu>
2019-11-20 17:37:12 +0100

    Removed unnecessary files

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
+	
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
@@ -14,20 +15,24 @@ public class AnalyticsCounter {
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		/**these 2 variables are useless
+		int i = 0;	// set i to 0 we never use i
+		int headCount = 0;	// counts headaches we already have headacheCount*/
 		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
+			//i++;	// increment i
+			//System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			//rush when it should rash for the equal
+			else if (line.equals("rash")) {
 				rashCount++;
+				System.out.println("number of rashes: " + rashCount);
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
+				System.out.println("number of pupils: " + pupilCount);
 			}
 
 			line = reader.readLine();	// get another symptom
