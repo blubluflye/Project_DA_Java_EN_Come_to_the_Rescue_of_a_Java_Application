773d279a8550828ec6bb63d2e0e2e93c171de23f
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2022-03-29 03:20:13 +0200

    write in result.out the unordored counted symptoms from symptoms.txt

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,56 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	List<String> symptomList;
 	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+	AnalyticsCounter(List<String> symptomList)
+	{
+		this.symptomList = symptomList;
+	}
+	public Map<String, Integer> getAnalytics()
+	{
+		Map<String, Integer> result = new HashMap<String, Integer>();
+		for (int i = 0; i < symptomList.size(); i++)
+		{
+			if ( result.get(symptomList.get(i)) == null)
+			{
+				result.put(symptomList.get(i), 1);
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
+			else
+			{
+				result.put(symptomList.get(i), result.get(symptomList.get(i)) + 1);
 			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		return result;
+	}
+
+	public static void main(String args[]) throws Exception {
+		ReadSymptomDataFromFile symptomList = new ReadSymptomDataFromFile("./Project02Eclipse/symptoms.txt");
+		AnalyticsCounter analytics = new AnalyticsCounter(symptomList.GetSymptoms());
+		Map<String, Integer> result = analytics.getAnalytics();
+		try
+		{
+			FileWriter writer = new FileWriter("result.out");
+			result.forEach((key, value) -> {
+				try {
+					writer.write(key + " " + value + "\n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			writer.close();					
+		}
+		catch (FileNotFoundException e) {
+	        throw e; // catch and re-throw
+	    } catch (IOException e) {
+	        System.err.print("Something went wrong");
+	    }
 	}
 }
